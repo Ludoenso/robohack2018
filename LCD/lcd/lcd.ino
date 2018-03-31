957ef42bfcc56d1e06b3d807ab9f4b5edf7709d8
Ludovic Vanasse <lvanasse@coginov.com>
2018-03-31 00:01:53 -0400

    Added warning for the  humidity and for the temp

--- LCD/lcd/lcd.ino
+++ LCD/lcd/lcd.ino
@@ -3,6 +3,10 @@
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 
+const int MAX_HUM = 25;
+
+const int MAX_TEMP = 25;
+
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 dht DHT;
 
@@ -11,19 +15,27 @@ dht DHT;
 void setup() {
  
  lcd.begin(16, 2);
+
+ DHT.read11(DHT11_PIN);
+
+ lcd.print("H :" + String(DHT.humidity));
+
+ lcd.setCursor(0, 1);
+ 
+ lcd.print("T :" + String(DHT.temperature));
  
 }
 
 void loop() {
+ lcd.begin(16, 2);
 
- lcd.setCursor(0, 0);
+ DHT.read11(DHT11_PIN); 
  
- DHT.read11(DHT11_PIN);
-
- lcd.print("Humidity : " + String(DHT.humidity));
+ lcd.print("H :" + String(DHT.humidity) + ((DHT.humidity > MAX_HUM) ?  "!!!": ""));
 
  lcd.setCursor(0, 1);
  
- lcd.print("Temp : " + String(DHT.temperature));
+ lcd.print("T :" + String(DHT.temperature) + ((DHT.temperature > MAX_TEMP) ?  "!!!": ""));
 
+ delay(1000);
 }
